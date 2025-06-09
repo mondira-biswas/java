ec947c0b08e32fe7e2ecff9ee02268cf7695552f
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:19:18 +0600

    Add files via upload

---
+++ MainSum.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class MainSum {
+    
+    
+        public static void main(Strings[] args) {
+            Scanner sc =new  Scanner(System.in);
+            int a  = sc.nextInt();
+            int b  = sc.nextInt();
+            int sum = a+b;
+            System.out.println(sum);
+    
+
+           
+        }
+    
+
+
+    
+}
