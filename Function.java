6b9907b6f9a2a4592702e423a63a073da01cbae8
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:14:21 +0600

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Function {
+    public static void printMyName(Strings name)
+    {
+        System.out.println(name);
+        return;
+    }
+    public static void main(Strings args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        Strings name =sc.next();
+        printMyName(name);
+    }
+    
+}
