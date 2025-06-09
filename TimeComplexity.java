6ea917744b4e670990213dc8f6984a19e6134179
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:30:36 +0600

    Add files via upload

---
+++ TimeComplexity.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class TimeComplexity {
+    public static void main(Strings args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int m=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                System.out.println("hello");
+            }
+        }
+    }
+    
+}
