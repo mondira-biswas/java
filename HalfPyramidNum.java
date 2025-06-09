cb9283f80225e6495d32bf731d545574f77bb45f
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:15:25 +0600

    Add files via upload

---
+++ HalfPyramidNum.java
@@ -0,0 +1,18 @@
+public class HalfPyramidNum {
+    public static void main(Strings args[])
+    {
+        int n = 5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+
+        }
+    
+    }
+
+    
+}
