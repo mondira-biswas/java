03dfa3edde99039e7320ecf9736bf18ee2d70d47
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:11:19 +0600

    Add files via upload

---
+++ Conditons.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Conditons {
+    public static void main(Strings args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x % 2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
