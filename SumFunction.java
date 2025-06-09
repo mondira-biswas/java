b2b97bbedcd84fcdcdf819011071e6f41df0ba95
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:29:11 +0600

    Add files via upload

---
+++ SumFunction.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SumFunction {
+ 
+    public static int  CalculateSum(int a, int b)
+    {
+        return a+b;
+    
+    }
+    
+    public static void main(Strings args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = CalculateSum(a,b);
+        System.out.println(sum);
+    }
+    
+}
+
+
+    
+
