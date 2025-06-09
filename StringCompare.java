b147532c3dc728b827e0bca1a8bec31371e4b99f
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:27:21 +0600

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StringCompare {
+public static void main(String args[])
+{
+    //compare
+    String sentence = "TonyStark";
+    String name = sentence.substring(5,7);
+    System.out.println(name);
+}
+    
+}
