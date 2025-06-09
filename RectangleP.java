1ffd4d915776c8891667a522179cb945e7205384
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:21:37 +0600

    Add files via upload

---
+++ RectangleP.java
@@ -0,0 +1,19 @@
+public class RectangleP {
+    public static void main(Strings args[])
+    {
+        int n = 4;
+        int m = 5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    
+    }
+
+    
+}
