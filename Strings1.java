44712eff420f01a7386f311ff9c89425202ed47a
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:28:43 +0600

    Add files via upload

---
+++ Strings1.java
@@ -0,0 +1,16 @@
+public class Strings1 {
+    public static void main(String args[])
+    {
+        String firstName  = "tony";
+        String lastName = "stark";
+        String fullName = "tony" + "@" +"stark";
+        System.out.println(fullName.length());
+    
+    //charAt
+    for(int i=0;i<fullName.length();i++)
+    {
+System.out.println(fullName.charAt(i));
+    }
+}
+    
+}
