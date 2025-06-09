19fe9ae458c60448525ceb26c91b63d2a7c43506
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:15:00 +0600

    Add files via upload

---
+++ HalfPyramid.java
@@ -0,0 +1,22 @@
+public class HalfPyramid {
+   
+        public static void main(Strings args[])
+        {
+            int n = 4;
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+    
+            }
+        
+        }
+    
+        
+    }
+    
+    
+
