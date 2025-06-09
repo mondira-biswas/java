8625f0bf11a1d11fc58f5bd814c8f6c12bae625c
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:23:26 +0600

    Add files via upload

---
+++ spaceH.java
@@ -0,0 +1,36 @@
+public class spaceH {
+    
+        public static void main(Strings args[])
+        {
+            int n = 4;
+    
+            //space
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n-i;j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++)
+                {
+                System.out.print("*");
+                }
+                System.out.println();
+    
+            }
+        
+        }
+    
+    
+        
+    
+    
+    
+
+
+
+    
+
+
+    
+}
