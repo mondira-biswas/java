6cab574511369e3ebcdb289b4b478f201a7cbdcc
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:17:54 +0600

    Add files via upload

---
+++ MainName.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+
+public class MainName {
+    public static void main(Strings[] args) {
+        Scanner sc = new Scanner(System.in);
+        Strings name = sc.nextLine();  // ✅ Fix here
+        System.out.println(name);
+    }
+}
