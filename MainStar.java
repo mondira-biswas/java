a1e8ae6833801f7dbcc6ea18c3ef8b037444d530
Mondira Biswas <biswasmondira57@gmail.com>
2025-06-09 10:18:28 +0600

    Add files via upload

---
+++ MainStar.java
@@ -0,0 +1,10 @@
+public class MainStar {
+    
+        public static void main(Strings[] args) {
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+        }
+    
+}
